615259a78d8af9487046b454f85077b2ddb9bb08
Ruslan Shakhaev <ruslan-shahaev@mail.ru>
2018-10-29 17:22:04 +0300

    One thread one proc

---
+++ src/client/socket.h
@@ -0,0 +1,53 @@
+//
+// Created by ruslan on 29.10.18.
+//
+
+#ifndef CLIENT_SOCKET_H
+#define CLIENT_SOCKET_H
+
+
+#include <string>
+#include <unistd.h>     // close()
+#include <memory>
+#include <functional>
+
+std::string int2ipv4(uint32_t ip);
+
+class Socket {
+public:
+    Socket()       : m_Sd(-1) {}
+    Socket(int sd) : m_Sd(sd) {}
+    ~Socket()                 { if (m_Sd > 0) ::close(m_Sd); }
+
+public:
+    int  sd() const noexcept { return m_Sd; }
+    void connect(const std::string &host, int port) noexcept(false);
+    void connect(const std::string &host, int port, int timeout) noexcept(false);
+    void send(const std::string &s) noexcept(false);
+    bool hasData() noexcept(false);
+    std::string recv() noexcept(false);
+    std::string recv(size_t bytes) noexcept(false);
+    std::string recvTimed(int timeout) noexcept(false);
+    void setRcvTimeout(int sec, int microsec) noexcept(false);
+    void setNonBlocked(bool opt) noexcept(false);
+    void setReuseAddr(int sd) noexcept(false);
+    void createServerSocket(uint32_t port, uint32_t queue_size) noexcept(false);
+    std::shared_ptr<Socket> accept() noexcept(false);
+    void close() { ::close(m_Sd); }
+
+    void httpQuery(const std::string &query, std::function<void(const std::string &s)> cb) noexcept(false) {
+        send(query);
+        std::string res = recv();
+        cb(res);
+    }
+
+private:
+    //Socket(const Socket &s);
+    //const Socket& operator=(const Socket &s);
+private:
+    int m_Sd;
+};
+
+
+
+#endif //CLIENT_SOCKET_H
