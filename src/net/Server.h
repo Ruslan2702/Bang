a01321395fdc984b301644fd0fb9d02e61acf9a2
a.baronskiy <a.baronskiy@mail.ru>
2018-11-12 19:32:49 +0300

    servers func add

--- src/net/Server.h
+++ src/net/Server.h
@@ -13,6 +13,8 @@
 #include "../logic/GameSituation.h"
 #include "../logic/Step.h"
 
+const int END_GAME = -1;
+
 void client_work(std::shared_ptr<Socket> client);
 
 void send_hellow_msg(std::vector<std::shared_ptr<Socket>> const &clients);
@@ -25,7 +27,13 @@ Step get_step(std::shared_ptr<Socket>);
 
 void new_cards_for_user(GameSituation &curren_stuation);
 
-void upgrade_situation(GameSituation &curren_stuation, Step const &step);
+int upgrade_situation(GameSituation &curren_stuation, Step const &step);  /// -1, если игра закончилась
+
+void send_end_msg(std::vector<std::shared_ptr<Socket>> const &clients);
+
+void insert_step_in_db(Step const &step);
+
+
 
 
 #endif //CPPPROJECT_SERVER_H
