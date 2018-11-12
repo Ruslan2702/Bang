99eeee906b35fef510b09f1e49609425d5da6398
a.baronskiy <a.baronskiy@mail.ru>
2018-11-12 20:22:45 +0300

    add f

--- src/net/Server.cpp
+++ src/net/Server.cpp
@@ -21,20 +21,20 @@ void send_hellow_msg(std::vector<std::shared_ptr<Socket>> const &clients) {
     }
 }
 
-void client_work(std::shared_ptr<Socket> client)
-{
-    client->setRcvTimeout(/*sec*/30, /*microsec*/0);
-    while (true) try
-        {
-            std::string line = client->recv();
-            client->send("echo: " + line + "\n");
-        }
-        catch(const std::exception &e)
-        {
-            std::cerr << "exception: " << e.what() << std::endl;
-            return;
-        }
-}
+//void client_work(std::shared_ptr<Socket> client)
+//{
+//    client->setRcvTimeout(/*sec*/30, /*microsec*/0);
+//    while (true) try
+//        {
+//            std::string line = client->recv();
+//            client->send("echo: " + line + "\n");
+//        }
+//        catch(const std::exception &e)
+//        {
+//            std::cerr << "exception: " << e.what() << std::endl;
+//            return;
+//        }
+//}
 
 int main(int argc, char *argv[]) /// запуск сервера
 {
