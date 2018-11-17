cecf01640c80517ec62d6d764839568d8e5e994a
a.baronskiy <a.baronskiy@mail.ru>
2018-11-17 13:12:13 +0300

    qwerty

---
+++ googletest-build/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/andrey/TECHNOPARK_SYSARC/SEM_01/CppProject/googletest-src/googletest
+# Build directory: /Users/andrey/TECHNOPARK_SYSARC/SEM_01/CppProject/googletest-build/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
